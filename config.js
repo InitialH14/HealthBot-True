baee4df5232a8903e3d85b2458366adc24a3533f
Hadid Ramadhan <137972935+InitialH14@users.noreply.github.com>
2024-06-18 11:27:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    newsApiURL : `https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=022447489dce49a88615a0f29890d98d`,
-    GPT_KEY: "sk-proj-WMZoQwoS2smZ9tqGcsa3T3BlbkFJKXDhKoWT4xPVoQ0BnvsT"
+    newsApiURL : ``,
+    GPT_KEY: ""
    }
 
 export default config
