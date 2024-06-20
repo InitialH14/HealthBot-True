5a4b9b10cd1511b99c0a054827961b4ca277c571
InitialH14 <anaksholehhf@gmail.com>
2024-06-20 12:02:54 +0700

    set config file

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    newsApiURL : "https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=022447489dce49a88615a0f29890d98d",
-    GPT_KEY: "sk-proj-dHCUfIHB8SRE6YKvZereT3BlbkFJhs9wCNLLukpX7sIWn3HQ"
+    newsApiURL : "",
+    url: ""
    }
 
 export default config
